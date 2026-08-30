e5a73aa4b67bb54fd6f0faab0092193691e2f233
Tamer <tamerinale@gmail.com>
2026-08-30 13:54:40 +0100

    Restore new dashboard (full-screen single page) as requested

---
+++ lib/core/breakpoints.dart
@@ -0,0 +1,16 @@
+﻿import 'package:flutter/widgets.dart';
+
+class Breakpoints {
+  static const double small = 600;
+  static const double medium = 900;
+  static const double large = 1200;
+
+  static bool isSmall(BuildContext context) => MediaQuery.sizeOf(context).width < small;
+  static bool isMedium(BuildContext context) {
+    final w = MediaQuery.sizeOf(context).width;
+    return w >= small && w < medium;
+  }
+  static bool isWide(BuildContext context) => MediaQuery.sizeOf(context).width >= medium;
+  static bool isLarge(BuildContext context) => MediaQuery.sizeOf(context).width >= large;
+}
+
